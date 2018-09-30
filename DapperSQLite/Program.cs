6c46b63bf5bad88ad90a48404dfaf8d892059d12
jcsonder <jcsonder@gmail.com>
2018-09-30 07:55:23 +0200

    Add Id field

--- DapperSQLite/Program.cs
+++ DapperSQLite/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SQLite;
+using Dapper;
 
 namespace DapperSQLite
 {
@@ -11,13 +12,11 @@ namespace DapperSQLite
 
             SQLiteConnection.CreateFile("MyDatabase.sqlite");
 
-            using (SQLiteConnection m_dbConnection = SetupConnection())
+            using (SQLiteConnection connection = SetupConnection())
             {
-                CreateDatabase(m_dbConnection);
-
-                SeedData(m_dbConnection);
-
-                ReadData(m_dbConnection);
+                CreateDatabase(connection);
+                SeedData(connection);
+                ReadData(connection);
             }
 
             Console.ReadLine();
@@ -25,38 +24,53 @@ namespace DapperSQLite
 
         private static SQLiteConnection SetupConnection()
         {
-            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
-            m_dbConnection.Open();
-            return m_dbConnection;
+            SQLiteConnection connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            connection.Open();
+            return connection;
         }
 
-        private static void SeedData(SQLiteConnection m_dbConnection)
+        private static void CreateDatabase(SQLiteConnection connection)
+        {
+            string sql = "create table highscores (id integer primary key autoincrement, name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+
+        private static void SeedData(SQLiteConnection connection)
         {
             string sql = "insert into highscores (name, score) values ('Me', 3000)";
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
             command.ExecuteNonQuery();
             sql = "insert into highscores (name, score) values ('Myself', 6000)";
-            command = new SQLiteCommand(sql, m_dbConnection);
+            command = new SQLiteCommand(sql, connection);
             command.ExecuteNonQuery();
             sql = "insert into highscores (name, score) values ('And I', 9001)";
-            command = new SQLiteCommand(sql, m_dbConnection);
+            command = new SQLiteCommand(sql, connection);
             command.ExecuteNonQuery();
         }
 
-        private static void CreateDatabase(SQLiteConnection m_dbConnection)
+        private static void ReadData(SQLiteConnection connection)
         {
-            string sql = "create table highscores (name varchar(20), score int)";
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-        }
-
-        private static void ReadData(SQLiteConnection m_dbConnection)
-        {
-            string sql = "select * from highscores order by score desc";
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            string query = "select * from highscores order by score desc";
+            SQLiteCommand command = new SQLiteCommand(query, connection);
             SQLiteDataReader reader = command.ExecuteReader();
             while (reader.Read())
-                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+                Console.WriteLine("Id: " + reader["id"] + "\tName: " + reader["name"] + "\tScore: " + reader["score"]);
+        }
+
+        //private static void ReadStructuredData(SQLiteConnection connection)
+        //{
+        //    string query = "select * from highscores order by score desc";
+        //    HighScore highScore = await connection.QuerySingleAsync<HighScore>(query, new { Id = id });
+
+        //    Console.WriteLine(string.Format("{0}-{1}-{2}", highScore.Id, highScore.Name, highScore.Score);
+        //}
+
+        public class HighScore
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Score { get; set; }
         }
     }
 }
